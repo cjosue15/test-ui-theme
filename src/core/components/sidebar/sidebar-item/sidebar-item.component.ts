51051c8a4c154bfce0ef1513f1075c1fc037aaa8
Josue <dev.carlos.morales@gmail.com>
2022-11-13 21:06:42 -0500

    feat(sidebar item): add functionallity to item

--- src/core/components/sidebar/sidebar-item/sidebar-item.component.ts
+++ src/core/components/sidebar/sidebar-item/sidebar-item.component.ts
@@ -1,15 +1,9 @@
 import { CommonModule } from '@angular/common';
-import {
-  Component,
-  EventEmitter,
-  Input,
-  Output,
-  OnInit,
-  inject,
-} from '@angular/core';
-import { INavItem } from '@core/models';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { IconsCoreModule } from '@core/icons';
+import { INavItem } from '@core/models';
+import { SidebarService } from '@core/services/sidebar';
 import { filter } from 'rxjs/operators';
 
 @Component({
@@ -28,19 +22,16 @@ import { filter } from 'rxjs/operators';
 export class SidebarItemComponent implements OnInit {
   @Input() item: INavItem;
 
-  @Output() onSelectedItem: EventEmitter<void>;
-
   expanded: boolean;
 
   private _router = inject(Router);
 
-  constructor() {
+  constructor(private _sidebarService: SidebarService) {
     this.item = {
       iconName: '',
       title: '',
     };
     this.expanded = false;
-    this.onSelectedItem = new EventEmitter();
   }
 
   ngOnInit(): void {
@@ -51,24 +42,75 @@ export class SidebarItemComponent implements OnInit {
         )
       )
       .subscribe((route: NavigationEnd) => {
-        if (this._confirmUrlInChildren(this.item, route.url)) {
-          if (this.expanded) return;
-          this.expanded = true;
-        } else {
-          if (!this.expanded) return;
-          this.expanded = false;
-        }
+        this._confirmUrlInChildren(this.item, route.url)
+          ? this._open()
+          : this._close();
       });
+
+    this._sidebarService.onSelectedItem$.subscribe((clickedItem) => {
+      if (clickedItem && clickedItem.children) {
+        // Check if the clicked item is one of the children of this item
+        // or
+        // // Check if the url can be found in one of the children of this item
+        if (
+          this._confirmItemInChildren(this.item, clickedItem) ||
+          this._confirmUrlInChildren(this.item, this._router.url)
+        ) {
+          return;
+        }
+
+        // If the clicked item is not this item, collapse...
+        if (this.item !== clickedItem) {
+          this._close();
+        }
+      }
+    });
   }
 
   toggleExpanded(event: Event): void {
     event.preventDefault();
     event.stopPropagation();
-    // this.onSelectedItem.emit();
+
+    this._sidebarService.setSelectedItem(this.item);
+
     if (this.item.children?.length) {
       this.expanded = !this.expanded;
     }
-    console.log(this.expanded);
+  }
+
+  private _close(): void {
+    if (!this.expanded) return;
+    this.expanded = false;
+  }
+
+  private _open(): void {
+    if (this.expanded) return;
+    this.expanded = true;
+  }
+
+  private _confirmItemInChildren(parent: INavItem, item: INavItem): boolean {
+    const children = parent.children;
+
+    // Return false if parent don't have any children
+    if (!children) {
+      return false;
+    }
+
+    // Return true parent has the provided item in one of its children
+    if (children.indexOf(item) > -1) {
+      return true;
+    }
+
+    for (const child of children) {
+      if (child.children) {
+        // Call function again with child (for sub to sub item)
+        if (this._confirmItemInChildren(child, item)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 
   private _confirmUrlInChildren(parent: INavItem, url: string): boolean {
