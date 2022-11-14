68a45ff5855b71b7f7174205348e3e465dda0a9d
Josue <dev.carlos.morales@gmail.com>
2022-11-13 20:59:30 -0500

    feat(sidebar-secion): add styles and class

--- src/core/components/sidebar/sidebar-section/sidebar-section.component.ts
+++ src/core/components/sidebar/sidebar-section/sidebar-section.component.ts
@@ -1,28 +1,50 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { INavSection } from '@core/models';
+import { IconsCoreModule } from '@core/icons';
 
 @Component({
   selector: '[sidebar-section]',
   standalone: true,
-  imports: [CommonModule],
-  template: `<span>{{ sectionName }}</span>`,
+  imports: [CommonModule, IconsCoreModule],
+  template: `
+    <ng-container>
+      <span>{{ sectionName }}</span>
+      <i-feather name="more-horizontal"></i-feather>
+    </ng-container>
+  `,
   styles: [
     `
-      span {
-        font-size: 0.9rem;
-        font-weight: 500;
-        line-height: 1.5;
-        letter-spacing: 0.01rem;
+      :host {
         color: #676d7d;
-        margin: 30px 0 10px 30px;
-        display: block;
+
+        i-feather,
+        span {
+          margin: 30px 0 10px 30px;
+        }
+
+        i-feather {
+          width: 18px;
+          height: 18px;
+          display: none;
+        }
+
+        span {
+          font-size: 0.9rem;
+          font-weight: 500;
+          line-height: 1.5;
+          letter-spacing: 0.01rem;
+          display: block;
+        }
       }
     `,
   ],
+  host: {
+    class: 'navigation__section',
+  },
 })
 export class SidebarSectionComponent implements OnInit {
   @Input() sectionName: string;
+
   constructor() {
     this.sectionName = '';
   }
