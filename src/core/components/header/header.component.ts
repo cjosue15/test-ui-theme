9ef733067516092efaa2bbd4f3a8227e8597ed11
Josue <dev.carlos.morales@gmail.com>
2022-11-13 20:58:03 -0500

    feat: add toggle sidebar depends on mobile or desktop

--- src/core/components/header/header.component.ts
+++ src/core/components/header/header.component.ts
@@ -1,18 +1,49 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { IconsCoreModule } from '@core/icons';
+import { ConfigService } from '@core/services/core';
+import { SidebarService } from '@core/services/sidebar';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IconsCoreModule],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent implements OnInit {
+  private _expanded: boolean;
 
-  constructor() { }
+  @Input() isMobile: boolean;
 
-  ngOnInit(): void {
+  constructor(
+    private _configService: ConfigService,
+    private _sidebarService: SidebarService
+  ) {
+    this._expanded = true;
+    this.isMobile = false;
   }
 
+  ngOnInit(): void {
+    const sidebarOptions = this._configService.config?.layout.sidebar;
+    if (sidebarOptions) this._expanded = sidebarOptions.collapsed;
+  }
+
+  toggleSidebar(): void {
+    console.log(this.isMobile);
+    if (!this.isMobile) {
+      this._expanded = !this._expanded;
+      this._configService.setconfig({
+        layout: { sidebar: { collapsed: this._expanded } },
+      });
+    } else {
+      this._sidebarService.sidebarIsOpenInMobile = true;
+    }
+  }
 }
